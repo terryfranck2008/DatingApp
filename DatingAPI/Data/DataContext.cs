ff2216d6a7860888a5764284068d088fa5d1ceb2
Franck TEPI <terryfranck2008@gmail.com>
2019-11-21 17:12:15 +0100

    initial commit

---
+++ DatingAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Value { get; set; }
+    }
+}
